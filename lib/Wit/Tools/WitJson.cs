5d9472eb412e3af9b9bec2e849703d5406cbf23f
Hubertus Sack <hubertussack@outlook.com>
2022-10-28 17:50:33 +0200

    Adding json stuff

---
+++ lib/Wit/Tools/WitJson.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Wit.Tools
+{
+    public static class WitJson
+    {
+        private static JsonSerializerSettings Create()
+        {
+            var resolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+            var config = new JsonSerializerSettings
+            {
+                ContractResolver = resolver,
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            return config;
+        }
+
+        private static readonly JsonSerializerSettings Config = Create();
+
+        public static T Deserialize<T>(string text)
+        {
+            var obj = JsonConvert.DeserializeObject<T>(text, Config);
+            return obj;
+        }
+
+        public static string Serialize(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj, Config);
+            return json;
+        }
+    }
+}
