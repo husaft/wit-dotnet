54ac9aaf3e263a20d0bf8baf93a4cd7d3556ec09
Hubertus Sack <hubertussack@outlook.com>
2022-10-28 17:56:31 +0200

    Adding errors and constants

---
+++ lib/Wit/Core/WitConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Wit.Core
+{
+    public static class WitConstants
+    {
+        public const string DefaultApiVersion = "20220801";
+
+        public const string DefaultWitUrl = "https://api.wit.ai";
+    }
+}
---
+++ lib/Wit/Errors/WitError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Wit.Errors
+{
+    public sealed class WitError : Exception
+    {
+        public WitError(string message) : base(message)
+        {
+        }
+    }
+}
