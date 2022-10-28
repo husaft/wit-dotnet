af3a62ffdda4c0c5ca306ec64f2fcab9a4e9c6b6
Hubertus Sack <hubertussack@outlook.com>
2022-10-28 17:44:02 +0200

    Adding console log

---
+++ lib/Wit/Tools/WitLog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Wit.Tools
+{
+    public static class WitLog
+    {
+        private static ILoggerFactory GetFactory(LogLevel minLevel)
+        {
+            var provider = new ServiceCollection()
+                .AddLogging(builder => builder
+                    .SetMinimumLevel(minLevel)
+                    .AddConsole()
+                )
+                .BuildServiceProvider();
+            return provider.GetService<ILoggerFactory>();
+        }
+
+        public static ILogger<T> Create<T>(LogLevel minLevel
+            = LogLevel.Information)
+        {
+            var factory = GetFactory(minLevel);
+            return factory.CreateLogger<T>();
+        }
+
+        public static ILogger<WitClient> Create(LogLevel minLevel
+            = LogLevel.Information) => Create<WitClient>(minLevel);
+    }
+}
